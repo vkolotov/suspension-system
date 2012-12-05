d7e52925e59f7c6066783d430ee14a9108105664
meccup <vkolotov@gmail.com>
2012-12-06 00:18:23 +0400

    added magnitometer system

---
+++ AutomaticSuspensionSystem/src/MagnetometerSystem.h
@@ -0,0 +1,17 @@
+/*
+ * MagnetometerSystem.h
+ *
+ *  Created on: 06.12.2012
+ *      Author: vlad
+ */
+
+#ifndef MAGNETOMETERSYSTEM_H_
+#define MAGNETOMETERSYSTEM_H_
+
+class MagnetometerSystem {
+public:
+	MagnetometerSystem();
+	virtual ~MagnetometerSystem();
+};
+
+#endif /* MAGNETOMETERSYSTEM_H_ */
